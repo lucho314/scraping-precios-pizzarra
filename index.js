b35029c6e592b64f208b31088452fc446bb6c1ce
Luciano Zapata <lzapata@ater.gob.ar>
2023-08-16 10:28:23 -0300

    slint

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
-import { scrapeData } from "./src/scraping.js";
-import express from 'express';
+import { scrapeData } from './src/scraping.js'
+import express from 'express'
 
-const app = express();
-const PORT = process.env.PORT ?? 8080;
-
-
+const app = express()
+const PORT = process.env.PORT ?? 8080
 
 app.get('/valores', async (req, res) => {
-    try {
-      const data = await scrapeData();
-      res.json(data);
-    } catch (error) {
-      console.error('Error al obtener los datos :', error);
-      res.status(500).json({ error: 'Ocurrio un error al obtener los valores.' });
-    }
-  });
-  
+  try {
+    const data = await scrapeData()
+    res.json(data)
+  } catch (error) {
+    console.error('Error al obtener los datos :', error)
+    res.status(500).json({ error: 'Ocurrio un error al obtener los valores.' })
+  }
+})
 
-
-  app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-  });
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`)
+})
--- src/scraping.js
+++ src/scraping.js
@@ -1,66 +1,67 @@
-import axios from 'axios';
-import {load} from 'cheerio';
-import { meses } from './constants.js';
+import axios from 'axios'
+import { load } from 'cheerio'
+import { meses } from './constants.js'
 
-const URL = 'https://www.cac.bcr.com.ar/es/precios-de-pizarra';
+const URL = 'https://www.cac.bcr.com.ar/es/precios-de-pizarra'
 
-async function fetchHTML() {
+async function fetchHTML () {
   try {
-    const { data } = await axios.get(URL);
-    return data;
+    const { data } = await axios.get(URL)
+    return data
   } catch (error) {
-    console.error('Error fetching the HTML:', error);
+    console.error('Error fetching the HTML:', error)
   }
 }
 
-export async function scrapeData() {
-  const html = await fetchHTML();
-  const $ = load(html);
+export async function scrapeData () {
+  const html = await fetchHTML()
+  const $ = load(html)
 
-  const precios = [];
-  const fechaActualizacion = $('.price-board-footer b').text().trim();
+  const precios = []
+  const fechaActualizacion = $('.price-board-footer b').text().trim()
 
-  const { fecha, hora } = extractFechaHora(fechaActualizacion);
+  const { fecha, hora } = extractFechaHora(fechaActualizacion)
 
   $('.board-wrapper').each((index, element) => {
-    const activo = $(element).find('h3').text().trim();
-    const precio = $(element).find('.price').text().trim();
-    const difTonelada = $(element).find('.cell:nth-child(2)').text().trim();
-    const difPorcentaje = $(element).find('.cell:nth-child(4)').text().trim();
-    const tendencia = $(element).find('.direction').hasClass('fa-arrow-up') ? 'up' : 'down';
+    const activo = $(element).find('h3').text().trim()
+    const precio = $(element).find('.price').text().trim()
+    const difTonelada = $(element).find('.cell:nth-child(2)').text().trim()
+    const difPorcentaje = $(element).find('.cell:nth-child(4)').text().trim()
+    const tendencia = $(element).find('.direction').hasClass('fa-arrow-up') ? 'up' : 'down'
 
     precios.push({
       activo,
       precio,
       difTonelada,
       difPorcentaje,
-      tendencia,
-    });
-  });
+      tendencia
+    })
+  })
 
-  return { activos: precios, info: { 
-    fechaActualizacion: fecha || '',
-    horaActualizacion: hora || '',
-   } };
+  return {
+    activos: precios,
+    info: {
+      fechaActualizacion: fecha || '',
+      horaActualizacion: hora || ''
+    }
+  }
 }
 
-
-
-function extractFechaHora(fechaHoraActualizacion) {
-  const regexFechaHora = /(\d{2}) de ([a-zA-Z]+) del (\d{4}) - Hora: (\d{2}:\d{2}) hs\./;
-  const matches = fechaHoraActualizacion.match(regexFechaHora);
+function extractFechaHora (fechaHoraActualizacion) {
+  const regexFechaHora = /(\d{2}) de ([a-zA-Z]+) del (\d{4}) - Hora: (\d{2}:\d{2}) hs\./
+  const matches = fechaHoraActualizacion.match(regexFechaHora)
 
   if (matches && matches.length === 5) {
-    const dia = matches[1];
-    const mes = matches[2];
-    const anio = matches[3];
-    const hora = matches[4];
-   
-    const mesNumero = meses[mes];
+    const dia = matches[1]
+    const mes = matches[2]
+    const anio = matches[3]
+    const hora = matches[4]
 
-    const fecha = `${dia}/${mesNumero}/${anio}`;
-    return { fecha, hora };
+    const mesNumero = meses[mes]
+
+    const fecha = `${dia}/${mesNumero}/${anio}`
+    return { fecha, hora }
   } else {
-    return { fecha: '', hora: '' };
+    return { fecha: '', hora: '' }
   }
 }
